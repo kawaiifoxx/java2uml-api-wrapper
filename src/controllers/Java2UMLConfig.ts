dbdfb175804d18beab09e1c454ce15058cc9613f
kawaiifoxx <shreyanshg06@gmail.com>
2021-07-15 22:47:06 +0530

    Defined Interface for Java2UMLConfig

---
+++ src/controllers/Java2UMLConfig.ts
@@ -0,0 +1,10 @@
+/**
+ * Specify Configuration for Java2UML
+ *
+ * @author kawaiifoxx
+ */
+export interface Java2UMLConfig {
+    baseURL: string
+    timeout: number
+    headers: object | null
+}
