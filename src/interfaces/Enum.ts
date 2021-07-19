c26f1de67381e1f74807b7f1c632c82049746bff
kawaiifoxx <shreyanshg06@gmail.com>
2021-07-19 18:32:16 +0530

    Updated interface definition

--- src/interfaces/Enum.ts
+++ src/interfaces/Enum.ts
@@ -2,7 +2,9 @@
  * An interface for representing Enum in j2u
  * @author kawaiifoxx
  */
-export interface Enum {
+import {LightWeight} from "./LightWeight";
+
+export interface Enum extends LightWeight {
     packageName: string
     body?: Body
 }
