23128ebc792d4706f3f07539c7e1dfbdbf489d3c
kawaiifoxx <shreyanshg06@gmail.com>
2021-07-19 18:32:23 +0530

    Updated interface definition

--- src/interfaces/Method.ts
+++ src/interfaces/Method.ts
@@ -1,13 +1,18 @@
 import {LightWeight} from "./LightWeight";
+import {Param} from "./Param";
+import {TypeParam} from "./TypeParam";
+import {SpecifiedException} from "./SpecifiedException";
 
 /**
  * An interface representing Method in j2u
  */
 export interface Method extends LightWeight {
-    packageName: string
     returnType: string
     signature: string
     visibility: string
     isStatic: string
+    parameters: Array<Param>
+    typeParameters: Array<TypeParam>
+    specifiedExceptions: Array<SpecifiedException>
     body?: Body
 }
